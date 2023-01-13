bd5525588d96faf2812fa338bad0b395afcb060c
Aryan Goel <aryangoel6900@yahoo.in>
2023-01-13 15:57:24 +0530

    feat: added arrow functions to check isPrime and isArmstrong

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log(2 === '2')
 // use === always
 
 // wap - take input: check if prime or armstrong number
-
+/*
 function isPrime() {
     let num = prompt("Enter a positive number: ")
     num = parseInt(num)
@@ -37,21 +37,67 @@ function isPrime() {
     else
         console.log('Composite number')
 } 
-
+*/
+/*
 function isArmstrong() {
-    let sum = 0;
-    let num = prompt('Enter a three-digit positive integer: ');
-    let temp = num;
+    let sum = 0
+    let num = prompt('Enter a three-digit positive integer: ')
+    let temp = num
     while (temp > 0) {
-        let rem = temp % 10;
-        sum += rem * rem * rem;
-        temp = parseInt(temp / 10);
+        let rem = temp % 10
+        sum += rem * rem * rem
+        temp = parseInt(temp / 10)
     }
 
     if (sum == num) {
-        console.log('Armstrong!');
+        console.log('Armstrong!')
     }
     else {
-        console.log(`Not Armstrong`);
+        console.log(`Not Armstrong`)
     }
+}
+*/
+
+// const isPrime = () => true;
+
+
+
+const isArmstrong = () => {
+    let sum = 0
+    let num = prompt('Enter a three-digit positive number: ')
+    let temp = num
+
+    while(temp > 0) {
+        let rem = temp % 10
+        sum += rem * rem * rem
+        temp = parseInt(temp/10)
+    }
+
+    if (sum == num) {
+        console.log('Armstrong!')
+    }
+    else {
+        console.log(`Not Armstrong`)
+    }
+}
+
+const isPrime = () => {
+    let num = prompt("Enter a positive number: ")
+    num = parseInt(num)
+    let isPrime = true
+    if(num == 2)
+        isPrime = true
+    else if(num == 1)
+        console.log('Neither prime nor composite')
+    else {
+        for(let i = 2; i <= (num-1); i++) {
+            if(num % i == 0)
+                isPrime = false
+        }
+    }
+
+    if(isPrime)
+        console.log('Prime number')
+    else
+        console.log('Composite number') 
 }
